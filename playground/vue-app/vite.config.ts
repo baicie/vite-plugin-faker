e7cff0573df1bf980bc7c26cf2319c2d262e37cf
baicie <zl316546@gmail.com>
2025-08-01 16:02:36 +0800

    chore: update TypeScript to version 5.9.2 across multiple packages and enhance axios integration in the Vue app

--- playground/vue-app/vite.config.ts
+++ playground/vue-app/vite.config.ts
@@ -2,17 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import faker from 'vite-plugin-faker'
 // https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [vue(), faker()],
-    server: {
-      port: 3001,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [vue(), faker()],
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
       },
     },
-  }
+  },
 })
