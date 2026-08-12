376dfa5eef0b17568f96aa49b78073f9282d1772
baicie <2055572879@qq.com>
2026-08-12 13:02:06 +0800

    types(shared): preserve structured request queries

--- packages/faker-shared/src/type.ts
+++ packages/faker-shared/src/type.ts
@@ -76,7 +76,7 @@ export interface RequestRecord {
   url: string
   method: string
   headers: Record<string, string>
-  query?: Record<string, string>
+  query?: QueryObject
   body?: any
   response?: {
     statusCode: number
